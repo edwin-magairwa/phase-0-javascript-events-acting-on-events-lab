cb3aea3fd69c5dfe03a6f17a628db9694eacba8f
edwin-magairwa <edwinmagairwa@gmail.com>
2025-03-24 12:00:39 +0300

    added events

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) { // Ensure dodger stays within game field
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) { // Ensure dodger stays within game field
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
